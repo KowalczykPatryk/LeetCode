313c19288801d05bb285662ff1974fb9e2fc6572
KowalczykPatryk <149658230+KowalczykPatryk@users.noreply.github.com>
2024-12-14 14:14:22 +0100

    Time: 0 ms (100%), Space: 8.7 MB (30.84%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int original = x;
+        long int reversed = 0;
+        while (x > 0)
+        {   
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+};
