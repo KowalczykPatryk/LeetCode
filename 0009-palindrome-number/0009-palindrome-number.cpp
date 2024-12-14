23aa78440beeeda903f20b76fad3f8fe8abcb744
KowalczykPatryk <149658230+KowalczykPatryk@users.noreply.github.com>
2024-12-14 14:44:26 +0100

    Time: 0 ms (100%), Space: 8.5 MB (81.81%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,13 +2,28 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         int original = x;
-        long int reversed = 0;
-        while (x > 0)
-        {   
-            int digit = x % 10;
-            reversed = reversed * 10 + digit;
-            x /= 10;
+        // range of int -2,147,483,648 to 2,147,483,647
+        // range of unsigned int 0 to 4,294,967,295
+        if (x == 0)
+        {
+            return true;
         }
-        return original == reversed;
+        else if (x < 0)
+        {
+            return false;
+        }
+        else
+        {
+            unsigned int original = x;
+            unsigned int reversed = 0;
+            while (x > 0)
+            {   
+                int digit = x % 10;
+                reversed = reversed * 10 + digit;
+                x /= 10;
+            }
+            return original == reversed;
+        }
+
     }
 };
