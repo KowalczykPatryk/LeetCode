ec1cc52692e755f865e281c49b06d13824435991
KowalczykPatryk <149658230+KowalczykPatryk@users.noreply.github.com>
2025-02-17 14:31:24 +0100

    Time: 1 ms (6.71%), Space: 8.6 MB (17.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int ways = 0;
-        for (int k = 0; k <= n / 2; k++)
+        if (n == 0 || n == 1)
         {
-            ways += binomialCoefficient(n-k, k);
+            return 1;
         }
-        return ways;
+
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
         
-    }
-    long int binomialCoefficient(int n, int k)
-    {
-        if (k > n - k) {
-            k = n - k;  // Use symmetry property
-        }
-        long int res = 1;
-        for (int i = 0; i < k; ++i) {
-            res *= (n - i);
-            res /= (i + 1);
-        }
-        return res;
     }
 };
