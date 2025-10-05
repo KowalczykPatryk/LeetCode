8dd9ac85ec57eeb620ab0f60fa1fe46ab380268a
KowalczykPatryk <149658230+KowalczykPatryk@users.noreply.github.com>
2025-10-05 13:01:40 +0200

    Time: 75 ms (48.17%), Space: 9.6 MB (60.66%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int max_length = 0;
+        int start_index = 0;
+        int end_index = 0;
+        for (int i = 0; i < s.length() - 1; i++){
+            for (int j = s.length() - 1; j > i; j--)
+            {
+                if (s[i] == s[j] && j-i+1 > max_length)
+                {
+                    //found potential palindorome to be checked
+                    int potential_max_length = j-i+1;
+                    for (int k = 1; k <= (j - i) / 2; k++)
+                    {
+                        if (s[i+k] != s[j-k])
+                        {
+                            potential_max_length = 0;
+                            break;
+                        }
+                    }
+                    if (potential_max_length > max_length)
+                    {
+                        max_length = potential_max_length;
+                        start_index = i;
+                        end_index = j;
+                    }
+                }
+            }
+
+        }
+        string output_string = "";
+        for (int i = start_index; i <= end_index; i++)
+        {
+            output_string += s[i];
+        }
+        return output_string;
+    }
+};
